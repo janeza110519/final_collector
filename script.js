1639b1ddff593b232f2ef025f54cda22e253b68e
Beth17-dev <aprilbethsalapang30@gmail.com>
2026-05-07 11:05:06 +0800

    update dashbboard

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ loginForm.addEventListener("submit", function(event) {
 registerBtn.addEventListener("click", function() {
     alert("Redirecting to Register Page...");
 });
+
+/* dashboard */
+const loginForm = document.getElementById("loginForm");
+const registerBtn = document.getElementById("registerBtn");
+
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(username === "admin" && password === "1234") {
+        alert("Login Successful!");
+    } else {
+        alert("Invalid Username or Password");
+    }
+});
+
+registerBtn.addEventListener("click", function() {
+    alert("Redirecting to Register Page...");
+});
