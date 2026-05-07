77d7025d010d8a7ef0975eb4b36ef36e338a01e4
janeza051911 <janezamalaya42gmail.com>
2026-05-07 10:57:29 +0800

    login

---
+++ script.js
@@ -0,0 +1,19 @@
+const loginForm = document.getElementById("loginForm");
+const registerBtn = document.getElementById("registerBtn");
+
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(username === "admin" && password === "1234") {
+        alert("Login Successful!");
+    } else {
+        alert("Invalid Username or Password");
+    }
+});
+
+registerBtn.addEventListener("click", function() {
+    alert("Redirecting to Register Page...");
+});
